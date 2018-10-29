aa3e2c3a40e2d794cd165d63455bc96346fe2ad2
james <jamesvipwot@hotmail.com>
2018-10-29 20:00:14 +0800

    OCR RESULT WRITE INTO FILES

--- Python/OCR/OCR.py
+++ Python/OCR/OCR.py
@@ -2,6 +2,13 @@ import pytesseract
 from PIL import Image
 
 
-img = Image.open('./Capture.PNG')
-code = pytesseract.image_to_string(img,lang='chi_sim')
+img = Image.open("F:\\SourceCode\\Python\\OCR\\Capture.PNG")
+code = pytesseract.image_to_string(img, lang="chi_sim")
 print(code)
+try:
+    with open(
+        "F:\\SourceCode\\Python\\OCR\\1.txt".encode("utf-8"), "w", encoding="utf-8"
+    ) as f:
+        f.write(code)
+except IOError as e:
+    print("FILI error:", str(e))
